cd61623094137ae1fc6d2236aa3d55f6977e5eaf
Linda Marko <lindamarko83@gmail.com>
2022-06-08 15:38:57 +0200

    Add https server and cert

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const cert = fs.readFileSync(path.join(__dirname, 'cert', 'localhost+1.pem'));
+const key = fs.readFileSync(
+  path.join(__dirname, 'cert', 'localhost+1-key.pem')
+);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', '/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile('./index.html', { root: '../build' });
+});
+
+https
+  .createServer({ key, cert }, app)
+  .listen(8000, console.log('Server running...'));
